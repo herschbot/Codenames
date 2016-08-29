c35344d9786f0d2d76fefaab04c056900bc45227
Herschel Pecker <herschel.pecker@yahoo.com>
2016-08-29 00:37:35 -0400

    continuing to fix bad firebase syntax

--- js/index.js
+++ js/index.js
@@ -87,22 +87,33 @@ function renderBoard(snapshot) {
 	});
 }
 
-function renderClue(snapshot) {
-	var clueNum = firebase.database().ref('clues').keys().length;
-	var passThis = {
-		clue: snapshot.child('clue').val(),
-		parity: (clueNum % 4 == 1 || clueNum % 4 == 2) ? 'odd' : 'even',
-		checkboxes : []
-	};
-	var team = snapshot.child('team').val().toLowerCase();
-	var color = team.charAt(0);
-	for (i = 1; i <= snapshot.child('number').val(); i++) {
-		passThis.checkboxes.push({
-			checkboxname : color + clueNum + i
-		});
-	}
-	var newClue = clueTemplate(passThis);
-	$('#' + team + 'clues').append(newClue);
+function renderClue(newClueSnap) {
+	firebase.database().ref('clues').on('value',function (allCluesSnap){
+		firebase.database().ref('clues').off('value');
+		clueNum = Object.keys(allCluesSnap.val());
+		var passThis = {
+			clue: newClueSnap.val().clue,
+			parity: (clueNum % 4 == 1 || clueNum % 4 == 2) ? 'odd' : 'even',
+			checkboxes: []
+		};
+		var team = newClueSnap.val().team.toLowerCase();
+		var color = team.charAt(0);
+		for (var i = 1; i<= newClueSnap.val().number; i++) {
+			passThis.checkboxes.push({
+				checkboxname : color + clueNum + i
+			});
+		}
+		var newClue = clueTemplate(passThis);
+		$('#' + team + 'clues').append(newClue);
+	});
+}
+
+function showClueEntry() {
+	$('#clueform').html(clueForm);
+}
+
+function hideClueEntry() {
+	$('#clueform').html('');
 }
 
 //Controller
@@ -125,92 +136,93 @@ function signedInSetup() {
 	firebase.database().ref('gameStats').child('turn').on('value',playTurn);
 }
 
-function playTurn(snapshot) {
+function playTurn(turnSnapshot) {
+	console.log('turn starting');
 	var userID = firebase.auth().currentUser.uid;
-	var player = firebase.database().ref('players').child(userID);
-
-	if (player.turn == snapshot.val()) {
-		if (player.role == 'Guesser') {
-			$('#wordgrid').on('click','td',revealTeam);
+	firebase.database().ref('players').child(userID).on('value', function (mySnapshot) {
+		if (mySnapshot.val().turn == turnSnapshot.val() ) {
+			console.log('your turn, in fact');
+			if (mySnapshot.val().role == 'Guesser') {
+				$('#wordgrid').on('click','td',revealTeam);
+			}
+			if (mySnapshot.val().role == 'Cluemaster') {
+				showClueEntry();
+				$('#clueform').on('click','#giveclue',handleClue);
+			}
 		}
-		if (player.role == 'Cluemaster') {
-			$('#clueform').html(clueForm);
-			$('#clueform').on('click','submit',handleClue);
-		}
-		
-	}
+	});
 }
 
 function handleClue() {
+	console.log('handling clue');
 	var clue = $('input[name="clue"').val();
 	var number = $('input[name="number"').val();
 
-	//this seems roundabout. Can we assume that this function only runs for current player?
-	var gameStats = firebase.database.ref('gameStats');
-	var turn = gameStats.child('turn').val();
-	var currentPlayer = gameStats.child('turnOrder').child(turn).val();
-	var team = firebase.database().ref('players').child(currentPlayer).child('team').val();
+	firebase.database().ref('gameStats').on('value',function (snapshot) {
+		//should be 0 or 2, (red or blue)
+		var turn = snapshot.val().turn;
+		var team = turn ? 'Blue' : 'Red';
+		firebase.database().ref('clues').push({
+			clue: clue,
+			number: number,
+			team: team
+		}, function () {
+			$('#clueform').off('click','submit');
+			hideClueEntry();
+			firebase.database().ref('gameStats').off('value');
+			firebase.database().ref('gameStats').child('guesses').update(number + 1);
+			firebase.database().ref('gameStats').child('turn').update( (turn + 1) % 4);
+		});
 
-	firebase.database().ref('clues').push({
-		clue: clue,
-		number: number,
-		team: team
-	},function() {
-		$('#clueform').off('click','submit');
-		$('#clueform').html('');
-		gameStats.child('turn').update( (turn + 1) % 4);
 	});
 }
 
 function revealTeam() {
 	var wordId = $(this).attr('id');
 	var word = firebase.database().ref('words').child(wordId);
-	var gameStats = firebase.database.ref('gameStats');
-	var guesses = gameStats.child('guesses');
-	var score = gameStats.child('score');
+	var gameStats = firebase.database().ref('gameStats');
+	word.on('value',function (wordSnap) {
+		word.off('value');
+		gameStats.on('value',function (gameSnap) {
+			gameStats.off('value');
+			var guesses = gameSnap.val().guesses;
+			var score = gameSnap.val().score;
 
-	//if has guesses left and the word's not been guessed
-	if (guesses.val() && !word.child(isGuessed).val()) {
-		//one less guess
-		guesses.update(guesses.val() - 1);
-		//reveal word in Model
-		word.child(isGuessed).update(true);
-		//update score
-		switch (word.child('team').val()) {
-			case 'Red':
-				score.update({
-					Red: score.Red.val() - 1
-				});
-				break;
-			case 'Blue':
-				score.update({
-					Blue: score.Blue.val() - 1
-				});
-				break;
-			case 'Assassin':
-				if (firebase.database().ref('gameStats').child('turn').val() == 1) {
-					score.update({
-						Blue: 0
-					});				
-				} else {
-					score.update({
-						Red: 0
-					});	
+			//if has guesses left and the word's not been guessed
+			if (guesses && !wordSnap.val().isGuessed) {
+				//one less guess
+				guesses--;
+				gameStats.child('guesses').update(guesses);
+				//reveal word
+				word.child('isGuessed').update(true);
+				//update score
+				var wordTeam = wordSnap.val().team;
+				switch (wordTeam) {
+					case 'Red':
+					case 'Blue':
+						gameSnap.child('score').child(wordTeam).update(score[wordTeam] - 1);
+						break;
+					case 'Assassin':
+						if (gameSnap.val().turn == 1) {
+							gameSnap.child('score').child('Blue').update(0);
+						} else {
+							gameSnap.child('score').child('Red').update(0);
+						}
 				}
-		}
+			}
 
-		//this seems roundabout. Can we assume that this function only runs for current player?
-		var turn = gameStats.child('turn').val();
-		var currentPlayer = gameStats.child('turnOrder').child(turn).val();
-		var currentPlayerTeam = firebase.database().ref('players').child(currentPlayer).child('team').val();
-		//if the current player guessed the wrong color or is out of guesses
-		if (word.child(team).val() != currentPlayerTeam || !guesses.val() ) {
-			$('#wordgrid').off('click','td');
-			gameStats.child('turn').update( (turn + 1) % 4);
-		}
-	}
+			var turn = gameSnap.val().turn;
+			var playerTeam = (turn == 1) ? 'Red' : 'Blue';
 
-	renderBoard();
+			//if out of guesses or wrong guess, end turn
+			if (!guesses || playerTeam != wordTeam) {
+				$('#wordgrid').off('click','td');
+				gameStats.child('turn').update( (turn + 1) % 4);
+			}
+		});
+	});
+
+	//renderBoard();
 }
 
 function checkGameStart(snapshot) {
@@ -282,12 +294,12 @@ function initGame () {
 	//wipe old data
 	firebase.database().ref('gameStats').remove(function () {
 		firebase.database().ref('players').on('value',function (snapshot) {
+			firebase.database().ref('players').off('value');
 			var players = snapshot.val();
 			var playerKeys = Object.keys(players);
 			for (var i = 0; i < 4; i++) {
 				firebase.database().ref('gameStats').child('turnOrder').child(i).set(playerKeys[i]);
 			}
-			firebase.database().ref('players').off('value');
 		});
 	});
 
@@ -295,6 +307,7 @@ function initGame () {
 	if (Math.random() < 0.5) {
 		Model.teamOne = 'Red';
 		firebase.database().ref('gameStats').set({
+			guesses: 0,
 			turn: 0,
 			//declare score, or just calculate it for display?
 			score: {
@@ -307,6 +320,7 @@ function initGame () {
 	} else {
 		Model.teamOne = 'Blue';
 		firebase.database().ref('gameStats').set({
+			guesses: 0,
 			turn: 2,
 			score: {
 				Red: 8,
